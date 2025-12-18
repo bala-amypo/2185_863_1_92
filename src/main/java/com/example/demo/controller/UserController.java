f5e4dc23ac9245f4493093cb2f0fa9a61fd0dc4c
bala-amypo <bala@amypo.in>
2025-12-18 15:05:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Tag(name = "Users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{email}")
+    @Operation(summary = "Find user by email")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
+    }
+}
