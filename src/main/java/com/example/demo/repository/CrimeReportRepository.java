e7edb24558138f0ad251a210db7e0d875e53dc2e
bala-amypo <bala@amypo.in>
2025-12-25 12:28:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/CrimeReportRepository.java
+++ src/main/java/com/example/demo/repository/CrimeReportRepository.java
@@ -6,6 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CrimeReportRepository extends JpaRepository<CrimeReport, Long> {
+
     @Query("SELECT c FROM CrimeReport c WHERE c.latitude BETWEEN ?1 AND ?2 AND c.longitude BETWEEN ?3 AND ?4")
-    List<CrimeReport> findByLatLongRange(double minLat, double maxLat, double minLong, double maxLong);
+    List<CrimeReport> findByLatLongRange(
+            double minLat,
+            double maxLat,
+            double minLong,
+            double maxLong
+    );
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,13 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
-import java.util.List;
 
 public interface UserService {
-
-    User register(User user) throws Exception;
-
-    User findByEmail(String email) throws Exception;
-
-    List<User> getUsers();   // ✅ ADD THIS
+    User register(User user);
+    User findByEmail(String email);
 }
